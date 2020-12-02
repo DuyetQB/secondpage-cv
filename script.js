07f48ccf68b864a37c0064adb0a52792151460ed
Dinh Si Duyet <dinhsyduyet@gmail.com>
2020-12-02 14:59:50 +0700

    new commit

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 
+document.addEventListener("DOMContentLoaded" , function(){
+    let icon = document.querySelector(".small-icon");
 
+    let  nav = document.querySelector(".small-nav");
+    let current = "click1";
+    icon.onclick = function(){
+        if(current == "click1"){
+            this.innerHTML = "<i class = 'fas fa-stream'></i>";
+
+            nav.style.display = "block";
+            current = "click2";
+        }
+        else if(current == "click2"){
+
+            this.innerHTML = "<i class= 'fas fa-bars'></i>";
+            nav.style.display = "none";
+            current = "click1";
+        }
+    }
+},false)
  
  
 
